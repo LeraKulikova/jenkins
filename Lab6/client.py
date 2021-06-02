79730225772ddbb0b2703d2841f676dd98663efc
Valeri К <littlefrom@mail.ru>
2021-06-03 01:45:46 +0500

    Lab6

--- Lab6/client.py
+++ Lab6/client.py
@@ -1,5 +1,5 @@
-import requests
 import sentry_sdk
+import random
 from sentry_sdk import configure_scope
 
 sentry_sdk.init("https://dbf98bd7df8941848dc689f45176b3d3@o778448.ingest.sentry.io/5797914")
@@ -9,11 +9,8 @@ with configure_scope() as scope:
     scope.set_tag("Lab", "2")
 
 try:
-    #r=requests.get("https://127.0.0.1:5000/")
-    r1=requests.get("https://api.nasa.gov/planetary/apod?api_key=")
-    if r1.status_code != 200:
-        print("Connection error, logging")
-        raise requests.exceptions.ConnectionError("Connection error, logging")
-except requests.exceptions.ConnectionError as e:
+    while True:
+        print(1/random.randint(-5,5))
+except Exception as e:
     sentry_sdk.capture_exception(e)
     print(str(e))
