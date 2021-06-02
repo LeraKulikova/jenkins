231f44600b4cb107228cf25d9282f7ea374b0cfb
Valeri К <littlefrom@mail.ru>
2021-06-03 01:36:39 +0500

    Lab6

--- Lab6/client.py
+++ Lab6/client.py
@@ -3,7 +3,7 @@ import sentry_sdk
 from sentry_sdk import configure_scope
 import logging
 
-sentry_sdk.init("https://f8774516c21f40938838bfe22005ae38@o392992.ingest.sentry.io/5252335")
+sentry_sdk.init("https://dbf98bd7df8941848dc689f45176b3d3@o778448.ingest.sentry.io/5797914")
 
 with configure_scope() as scope:
     scope.set_tag("Project", "IKSiS")
